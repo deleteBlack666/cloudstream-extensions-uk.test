8dbf5174ee403c69220d6056659af9e7d6c9d8e0
deleteBlack666 <78680082+deleteBlack666@users.noreply.github.com>
2026-06-09 01:08:41 +0300

    Update AshdiExtractor.kt

--- AnitubeinuaProvider/src/main/kotlin/com/lagradost/extractors/AshdiExtractor.kt
+++ AnitubeinuaProvider/src/main/kotlin/com/lagradost/extractors/AshdiExtractor.kt
@@ -1,3 +1,7 @@
+package com.lagradost.extractors
+
+import com.lagradost.cloudstream3.app
+
 class AshdiExtractor() {
     suspend fun ParseM3U8(url: String): String {
         val scriptHtml = app.get(url).document.select("script").html()
