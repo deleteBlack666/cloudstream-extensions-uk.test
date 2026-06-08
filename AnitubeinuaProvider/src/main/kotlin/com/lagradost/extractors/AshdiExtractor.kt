b8cc25fdcca8832b90ba9ed5e6061720c5642da2
deleteBlack666 <78680082+deleteBlack666@users.noreply.github.com>
2026-06-08 23:58:35 +0300

    Update AshdiExtractor.kt

--- AnitubeinuaProvider/src/main/kotlin/com/lagradost/extractors/AshdiExtractor.kt
+++ AnitubeinuaProvider/src/main/kotlin/com/lagradost/extractors/AshdiExtractor.kt
@@ -3,10 +3,11 @@ package com.lagradost.extractors;
 import com.lagradost.cloudstream3.app
 
 class AshdiExtractor() {
-    suspend fun ParseM3U8(url: String): String{
-        return app.get(url).document.select("script").html()
-            .substringAfterLast("file:\"")
-            .substringBefore("\",")
-
+    suspend fun ParseM3U8(url: String): String {
+        val scriptHtml = app.get(url).document.select("script").html()
+        // Site uses single quotes: file:'https://...m3u8'
+        return scriptHtml
+            .substringAfterLast("file:'")
+            .substringBefore("'")
     }
 }
