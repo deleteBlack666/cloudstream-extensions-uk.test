3d37351746e521f75a48afbcddcf3a68c4b33628
deleteBlack666 <78680082+deleteBlack666@users.noreply.github.com>
2026-06-09 01:04:37 +0300

    Update AshdiExtractor.kt

--- AnitubeinuaProvider/src/main/kotlin/com/lagradost/extractors/AshdiExtractor.kt
+++ AnitubeinuaProvider/src/main/kotlin/com/lagradost/extractors/AshdiExtractor.kt
@@ -1,13 +1,9 @@
-package com.lagradost.extractors
-
-import com.lagradost.cloudstream3.app
-
-class AshdiExtractor {
-    suspend fun ParseM3U8(url: String, referer: String = "https://anitube.in.ua/"): String {
-       // Надійний пошук m3u8 за допомогою регулярного виразу
-        val regex = """file\s*:\s*['"](https?://[^'"]+\.m3u8.*?)['"]""".toRegex()
-        
-        return regex.find(html)?.groupValues?.get(1) 
-            ?: throw Exception("Не вдалося витягнути посилання (.m3u8) з плеєра Ashdi")
+class AshdiExtractor() {
+    suspend fun ParseM3U8(url: String): String {
+        val scriptHtml = app.get(url).document.select("script").html()
+        // Site uses single quotes: file:'https://...m3u8'
+        return scriptHtml
+            .substringAfterLast("file:'")
+            .substringBefore("'")
     }
 }
